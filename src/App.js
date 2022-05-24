ff4422ead93deadf7137d177dbbdc7f1a16a24bc
NikitaKhadnevich <vadim-swich@yandex.ru>
2022-05-24 15:57:01 +0300

    [conter] update devDep status

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import DevTools from 'mobx-react-devtools';
 
 import Counter from './pages/Counter';
 
 function App() {
-  return <Counter />;
+  return (
+    <div className='App'>
+      <DevTools />
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
